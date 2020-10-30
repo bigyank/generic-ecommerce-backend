2f003ab6e24c72c39a12f1a5149cc2900444811d
bigyank <this.is.blueorange@gmail.com>
2020-10-31 01:03:50 +0545

    added authorization middleware

---
+++ utils/authorization.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+const User = require('../models/userModel');
+const { JWT_SECRET } = require('./config');
+
+const authorize = async (req, _res, next) => {
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, JWT_SECRET);
+
+            req.user = await User.findById(decoded.id);
+
+            return next();
+        } catch (error) {
+            throw createError(401, 'Not Authorized');
+        }
+    }
+
+    throw createError(401, 'Not Authorized');
+};
+
+module.exports = authorize;
